3a5f7fdd6800837eeefdb516995c73e4608e5e90
creativ10 <mv5195@student.uni-lj.si>
2020-06-25 09:17:23 +0200

    fixed login backend

---
+++ ucbenik/serializers.py
@@ -0,0 +1,9 @@
+from .models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    first_name = serializers.CharField()
+    sex = serializers.CharField()
+    age = serializers.IntegerField()
--- ucbenik/views.py
+++ ucbenik/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from ucbenik.models import User
 from ucbenik.CustomAuth import CustomAuth
+from .serializers import UserSerializer
 
 lesson_one = {"Introduction": "introduction", "Exercises": "exercises", "Avatar": "avatar", "Numbers": "numbers",
               "Colors": "colors", "Years": "years"}
@@ -35,10 +36,8 @@ def login_page(request):
         password = request.POST['Password']
         user_check = CustomAuth()
         user = user_check.authenticate(username, password)
-        print(user)
         if user is not None:
-            print()
-            request.session['user'] = user.__dict__
+            request.session['user'] = UserSerializer(user).data
             return redirect("/lesson_one/introduction/page_one")
 
 
@@ -46,99 +45,99 @@ def introduction_page_one(request):
     print(request.session['user'])
     if request.method == "GET":
         return render(request, "lesson1/introduction/page_one.html", {"next": "/lesson_one/introduction/page_two", "back": "/", "lesson_one": lesson_one,
-                                                              "lesson": "Lesson 1: About Me", "title": "Introduction"})
+                                                                      "lesson": "Lesson 1: About Me", "title": "Introduction"})
 
 
 def introduction_page_two(request):
     if request.method == "GET":
         return render(request, "lesson1/introduction/page_two.html", {"next": "/lesson_one/introduction/page_three",
-                                                              "back": "/lesson_one/introduction/page_one", "lesson_one": lesson_one,
-                                                              "lesson": "Lesson 1: About Me", "title": "Introduction"})
+                                                                      "back": "/lesson_one/introduction/page_one", "lesson_one": lesson_one,
+                                                                      "lesson": "Lesson 1: About Me", "title": "Introduction"})
 
 
 def introduction_page_three(request):
     if request.method == "GET":
         return render(request, "lesson1/introduction/page_three.html", {"next": "/lesson_one/introduction/page_four",
-                                                              "back": "/lesson_one/introduction/page_two",
-                                                                "lesson_one": lesson_one,
-                                                                "lesson": "Lesson 1: About Me", "title": "Introduction"})
+                                                                        "back": "/lesson_one/introduction/page_two",
+                                                                        "lesson_one": lesson_one,
+                                                                        "lesson": "Lesson 1: About Me", "title": "Introduction"})
 
 
 def introduction_page_four(request):
     if request.method == "GET":
         return render(request, "lesson1/introduction/page_four.html", {"next": "/lesson_one/introduction/page_five",
-                                                              "back": "/lesson_one/introduction/page_three",
-                                                                "lesson_one": lesson_one,
-                                                               "lesson": "Lesson 1: About Me", "title": "Introduction"})
+                                                                       "back": "/lesson_one/introduction/page_three",
+                                                                       "lesson_one": lesson_one,
+                                                                       "lesson": "Lesson 1: About Me", "title": "Introduction"})
 
 
 def introduction_page_five(request):
     if request.method == "GET":
         return render(request, "lesson1/introduction/page_five.html", {"next": "/lesson_one/introduction/page_six",
-                                                              "back": "/lesson_one/introduction/page_four",
-                                                                "lesson_one": lesson_one,
-                                                               "lesson": "Lesson 1: About Me", "title": "Introduction"})
+                                                                       "back": "/lesson_one/introduction/page_four",
+                                                                       "lesson_one": lesson_one,
+                                                                       "lesson": "Lesson 1: About Me", "title": "Introduction"})
 
 
 def introduction_page_six(request):
     if request.method == "GET":
         return render(request, "lesson1/introduction/page_six.html", {"next": "/lesson_one/exercises/page_one",
-                                                              "back": "/lesson_one/introduction/page_five",
-                                                                "lesson_one": lesson_one,
-                                                              "lesson": "Lesson 1: About Me", "title": "Introduction"})
+                                                                      "back": "/lesson_one/introduction/page_five",
+                                                                      "lesson_one": lesson_one,
+                                                                      "lesson": "Lesson 1: About Me", "title": "Introduction"})
 
 
 def exercises_page_one(request):
     if request.method == "GET":
         return render(request, "lesson1/exercises/page_one.html", {"next": "/lesson_one/exercises/page_two",
-                                                              "back": "/lesson_one/introduction/page_six",
-                                                                "lesson_one": lesson_one,
-                                                              "lesson": "Lesson 1: About Me", "title": "Exercises"})
+                                                                   "back": "/lesson_one/introduction/page_six",
+                                                                   "lesson_one": lesson_one,
+                                                                   "lesson": "Lesson 1: About Me", "title": "Exercises"})
 
 
 def exercises_page_two(request):
     if request.method == "GET":
         return render(request, "lesson1/exercises/page_two.html", {"next": "/lesson_one/exercises/page_three",
-                                                              "back": "/lesson_one/exercises/page_one",
-                                                                "lesson_one": lesson_one,
-                                                              "lesson": "Lesson 1: About Me", "title": "Exercises"})
+                                                                   "back": "/lesson_one/exercises/page_one",
+                                                                   "lesson_one": lesson_one,
+                                                                   "lesson": "Lesson 1: About Me", "title": "Exercises"})
 
 
 def exercises_page_three(request):
     if request.method == "GET":
         return render(request, "lesson1/exercises/page_three.html", {"next": "/lesson_one/exercises/page_four",
-                                                              "back": "/lesson_one/exercises/page_two",
-                                                                "lesson_one": lesson_one,
-                                                              "lesson": "Lesson 1: About Me", "title": "Exercises"})
+                                                                     "back": "/lesson_one/exercises/page_two",
+                                                                     "lesson_one": lesson_one,
+                                                                     "lesson": "Lesson 1: About Me", "title": "Exercises"})
 
 
 def exercises_page_four(request):
     if request.method == "GET":
         return render(request, "lesson1/exercises/page_four.html", {"next": "/lesson_one/exercises/page_five",
-                                                              "back": "/lesson_one/exercises/page_three",
-                                                                "lesson_one": lesson_one,
-                                                              "lesson": "Lesson 1: About Me", "title": "Exercises"})
+                                                                    "back": "/lesson_one/exercises/page_three",
+                                                                    "lesson_one": lesson_one,
+                                                                    "lesson": "Lesson 1: About Me", "title": "Exercises"})
 
 
 def exercises_page_five(request):
     if request.method == "GET":
         return render(request, "lesson1/exercises/page_five.html", {"next": "/lesson_one/exercises/page_six",
-                                                              "back": "/lesson_one/exercises/page_four",
-                                                                "lesson_one": lesson_one,
-                                                              "lesson": "Lesson 1: About Me", "title": "Exercises"})
+                                                                    "back": "/lesson_one/exercises/page_four",
+                                                                    "lesson_one": lesson_one,
+                                                                    "lesson": "Lesson 1: About Me", "title": "Exercises"})
 
 
 def exercises_page_six(request):
     if request.method == "GET":
         return render(request, "lesson1/exercises/page_six.html", {"next": "/lesson_one/exercises/page_seven",
-                                                              "back": "/lesson_one/exercises/page_five",
-                                                                "lesson_one": lesson_one,
-                                                              "lesson": "Lesson 1: About Me", "title": "Exercises"})
+                                                                   "back": "/lesson_one/exercises/page_five",
+                                                                   "lesson_one": lesson_one,
+                                                                   "lesson": "Lesson 1: About Me", "title": "Exercises"})
 
 
 def exercises_page_seven(request):
     if request.method == "GET":
         return render(request, "lesson1/exercises/page_seven.html", {"next": "empty",
-                                                              "back": "/lesson_one/exercises/page_six",
-                                                                "lesson_one": lesson_one,
-                                                              "lesson": "Lesson 1: About Me", "title": "Exercises"})
+                                                                     "back": "/lesson_one/exercises/page_six",
+                                                                     "lesson_one": lesson_one,
+                                                                     "lesson": "Lesson 1: About Me", "title": "Exercises"})
