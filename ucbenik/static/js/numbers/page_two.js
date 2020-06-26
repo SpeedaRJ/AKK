603d7ca41b681424f33c1cf87ef7ab63856cca56
ValterH <valter.hudovernik@gmail.com>
2020-06-26 17:26:03 +0200

    Numbers page 1, 2

--- ucbenik/static/js/numbers/page_two.js
+++ ucbenik/static/js/numbers/page_two.js
@@ -1,11 +1,12 @@
+let colors = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+let hex_codes = ["1","2","3","4","5","6","7","8","9","10"];
+
 function toOrigin1(el) {
     var origin = document.getElementById('drag-origin');
     if (origin === el.parentElement) return;
     origin.appendChild(el);
     sortItems();
     checkCorrectness();
-    //delete dragDropWordsMapping[el.id];
-    //saveWordDrag();
 }
 
 function allowDrop(ev) {
@@ -14,12 +15,9 @@ function allowDrop(ev) {
 
 function drag(ev) {
     ev.dataTransfer.setData("Text", ev.target.id);
-    //var ow = $('#drop-name');
-    //ow.find('#nedokoncano').hide();
-    //ow.addClass('opora-done');
 }
 
-function drop(ev, el, limit, solution) {
+function drop(ev, el, limit) {
     if (el.children.length > limit) {
         try {
             children = el.children;
@@ -36,10 +34,10 @@ function drop(ev, el, limit, solution) {
         }
     }
     var origin = document.getElementById('drag-origin');
+
     ev.preventDefault();
     var data = ev.dataTransfer.getData("Text");
-    //data.indexOf(el.id)
-    correct = data == solution;
+    correct = data.search("color" + el.id.substring(el.id.length-1)) > -1;
     var child = document.getElementById(data);
     if (correct) {
         child.classList.add('correct');
@@ -50,8 +48,6 @@ function drop(ev, el, limit, solution) {
     }
     el.classList.add('full');
     el.appendChild(child);
-    //dragDropWordsMapping[data] = el.id;
-    //saveWordDrag();
     checkCorrectness();
 }
 
@@ -83,6 +79,37 @@ function checkCorrectness() {
     }
 }
 
+function setColors() {
+    let selected = [];
+    while (selected.length !== 5) {
+        let randomElement = colors[Math.floor(Math.random() * colors.length)];
+        if(!selected.includes(randomElement)) selected.push(randomElement);
+    }
+    let hex = [];
+    for(let color in selected) {
+        hex.push(hex_codes[colors.findIndex((element) => element === selected[color])]);
+    }
+    document.getElementById("match01").innerHTML = hex[0];
+    document.getElementById("match02").innerHTML = hex[1];
+    document.getElementById("match03").innerHTML = hex[2];
+    document.getElementById("match04").innerHTML = hex[3];
+    document.getElementById("match05").innerHTML = hex[4];
+    document.getElementById("match1").innerHTML = selected[0];
+    document.getElementById("match2").innerHTML = selected[1];
+    document.getElementById("match3").innerHTML = selected[2];
+    document.getElementById("match4").innerHTML = selected[3];
+    document.getElementById("match5").innerHTML = selected[4];
+}
+
+function redo(e) {
+    let node = [].slice.call(document.getElementsByClassName("draggable-word"));
+    if(node.some((el) => el.className.includes("correct")))
+        [].forEach.call(node, function(el) {el.click(); console.log(el)});
+    setColors();
+}
+
+
 $(function () {
     sortItems();
+    setColors();
 });
