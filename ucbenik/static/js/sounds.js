735cb6381f3dc54381e2070d4fd69449f58135b8
ValterH <valter.hudovernik@gmail.com>
2020-09-13 13:23:56 +0200

    minor

--- ucbenik/static/js/sounds.js
+++ ucbenik/static/js/sounds.js
@@ -8,7 +8,10 @@ function tts(id) {
     var msg = new SpeechSynthesisUtterance();
     msg.text = document.getElementById(id).value;
     if (!msg.text)
-      msg.text = "Please enter a value."
+    msg.text = "Please enter a value."
+    msg.volume = 0.5; // From 0 to 1
+    msg.rate = 1; // From 0.1 to 10
+    msg.pitch = 2; // From 0 to 2
     window.speechSynthesis.speak(msg);
    }else{
      // Speech Synthesis Not Supported 😣
