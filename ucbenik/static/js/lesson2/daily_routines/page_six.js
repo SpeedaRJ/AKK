fc473ae70827cf1c4132e24b48340d2c792a5ce7
creativ10 <mv5195@student.uni-lj.si>
2020-11-14 01:13:52 +0100

    fixed drag and drop on page 6 in daily routines

--- ucbenik/static/js/lesson2/daily_routines/page_six.js
+++ ucbenik/static/js/lesson2/daily_routines/page_six.js
@@ -1,9 +1,21 @@
-function toOrigin1(el) {
+function toOrigin(el) {
     var origin = document.getElementById('drag-origin');
     if (origin === el.parentElement) return;
+    var text = document.getElementById("sentence "+el.id);
+    console.log(text);
     origin.appendChild(el);
-    sortItems();
-    checkCorrectness();
+    var div_with_sentences = document.getElementById("sentences");
+    div_with_sentences.removeChild(text);
+    var draghere = document.getElementById("draghere");
+    console.log(draghere.children);
+    if(draghere.children.length === 0){
+        draghere.setAttribute("style","display: flex; align-items: center; justify-content: center;flex-direction: column;\n" +
+            "                flex-wrap: wrap; height: 450px; border-radius: 25px");
+        var text_2 = document.createElement("h3");
+        text_2.innerText = "Drag here!";
+        text_2.id ="DragHere";
+        draghere.appendChild(text_2);
+    }
 }
 
 function allowDrop(ev) {
@@ -11,86 +23,54 @@ function allowDrop(ev) {
 }
 
 function drag(ev) {
+    console.log(ev);
     ev.dataTransfer.setData("Text", ev.target.id);
 }
 
-function drop(ev, el, limit) {
-    if (el.children.length > limit) {
-        try {
-            children = el.children;
-            for(var i = 0; i < children.length; i++) {
-                if(children[i].classList !== undefined && children[i].className.includes("incorrect")) {
-                    toOrigin1(children[i]);
-                    break;
-                }
-            }
-            if(i === children.length && i > 1)
-                return;
-        } catch {
-            return;
-        }
+function drop(ev, el) {
+    var dragehere = document.getElementById("DragHere");
+    if(dragehere !== null){
+        el.removeChild(dragehere);
+        el.setAttribute("style","display: flex; flex-direction: column; height:  450px; border-radius: 25px;")
     }
-    var origin = document.getElementById('drag-origin');
-
     ev.preventDefault();
     var data = ev.dataTransfer.getData("Text");
-    correct = data.search(el.id.replace("0","")) > -1;
-    console.log(correct);
     var child = document.getElementById(data);
-    if (correct) {
-        child.classList.add('correct');
-        child.classList.remove('incorrect');
-    } else {
-        child.classList.add('incorrect');
-        child.classList.remove('correct');
-    }
-    el.classList.add('full');
     el.appendChild(child);
-    checkCorrectness();
+    var div_with_sentences = document.getElementById("sentences");
+    var text = document.createElement("p");
+    text.innerHTML = sentences[data];
+    text.id = "sentence " + data;
+    div_with_sentences.appendChild(text);
 }
-
-function checkCorrectness() {
-    let items = document.getElementsByClassName("draggable-word")
-    let counter = 0;
-    for(let x = 0; x < items.length; x++) {
-        if(items[x].classList !== undefined && items[x].className.includes("correct") && !items[x].className.includes("incorrect"))
-            counter++;
-    }
-    if(counter === items.length) {
-        document.getElementById("next").removeAttribute("disabled")
-    } else {
-         document.getElementById("next").setAttribute("disabled", "disabled");
-    }
-}
-
-$(document).ready(function() {
+var sentences = {};
+$(document).ready(function () {
     $.ajax({
         url: "/pictures",
-        success: function(result) {
+        success: function (result) {
             var pictures = document.getElementById("drag-origin");
             var count = 0;
-            for(var i in result.links) {
-                if(i.includes("0")){
+            for (var i in result.links) {
+                var temp = i;
+                if (i.includes("0")) {
                     var list = i.split("0");
-                    i = list[0] + list[1];
+                    temp = list[0] + list[1];
                 }
-                var child = document.createElement("div");
-                child.classList.add("zaj");
                 var obj = document.createElement("object");
                 obj.classList.add("img-visual");
                 obj.setAttribute("type", "image/svg+xml");
-                obj.setAttribute("data", "/static/" + i)
+                obj.setAttribute("data", "/static/" + temp);
+                obj.setAttribute("style","height:25px;");
                 var p = document.createElement("p");
-                p.innerHTML = result.links[i];
-                p.setAttribute("hidden", "hidden");
-                child.appendChild(obj);
-                child.appendChild(p);
-                child.setAttribute("ondragstart", "drag(event)");
-                child.setAttribute("onclick", "toOrigin1(this)");
-                child.setAttribute("id", "match" + count);
-                child.setAttribute("draggable", "true");
-                child.classList.add("draggable-word");
-                pictures.appendChild(child);
+                p.appendChild(obj);
+                p.setAttribute("ondragstart", "drag(event)");
+                p.setAttribute("onclick", "toOrigin(this)");
+                p.setAttribute("id", "match" + count);
+                p.setAttribute("draggable", "true");
+                p.classList.add("draggable-word");
+                p.setAttribute("style","margin:0; width:100;");
+                sentences[p.id] = result.links[i];
+                pictures.appendChild(p);
                 count++;
             }
         }
